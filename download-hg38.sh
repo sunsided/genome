633bab547856c24b006bafeb19a90c388b41f659
Markus Mayer <widemeadows@gmail.com>
2026-05-05 18:29:38 +0200

    Use parallel downloads only for hg38.fa.gz

--- download-hg38.sh
+++ download-hg38.sh
@@ -2,12 +2,22 @@
 
 set -euo pipefail
 
-mkdir -p hg38
-cd hg38
+BASE="https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips"
+OUT="hg38"
 
+mkdir -p "$OUT"
+cd "$OUT"
+
+# Big file: segmented + resumable.
 aria2c -x 8 -s 8 -c \
-  https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.fa.gz \
-  https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chrom.sizes \
-  https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.chromAlias.txt \
-  https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/md5sum.txt
+  "$BASE/latest/hg38.fa.gz"
+
+# Small files: no segmentation, no resume weirdness.
+aria2c -x 1 -s 1 --allow-overwrite=true \
+  "$BASE/hg38.chrom.sizes" \
+  "$BASE/hg38.chromAlias.txt" \
+  "$BASE/md5sum.txt"
+
+# Verify downloaded files that are present in md5sum.txt.
+md5sum -c md5sum.txt --ignore-missing
 
